07a0591823ee5ea1368862716ae9ea05abd43df8
Akash <akashghode1234@gmail.com>
2022-09-23 22:44:32 +0530

    assignment

---
+++ Assignments/Assignment_1/que_01.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")()
+
+// 1. Write a program to find whether a given year is a leap year or not.
+
+const year = prompt("Enter the Year: ");
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+    console.log(`${year} is a leap year`);
+} else {
+    console.log(`${year} is not a leap year`);
+}
