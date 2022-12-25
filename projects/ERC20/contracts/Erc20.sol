2764cb16ad6f7366573ada6ed7fe446c29e41e60
Akash <akashghode1234@gmail.com>
2022-12-25 23:29:58 +0530

    erc20-testing

--- projects/ERC20/contracts/Erc20.sol
+++ projects/ERC20/contracts/Erc20.sol
@@ -1,10 +1,40 @@
-// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import from node_modules @openzeppelin/contracts v4.0
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MNFT") {
+contract ICO is ERC20, Ownable {
+
+    constructor() ERC20("Drops", "DRP") {
+      _mint(msg.sender, 1000000*(10**uint256(decimals())));
+    }
+    
+
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+      require(account != address(0) && amount != uint256(0), "ERC20: function mint invalid input");
+      _mint(account, amount);
+      return true;
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner returns (bool) {
+      require(account != address(0) && amount != uint256(0), "ERC20: function burn invalid input");
+      _burn(account, amount);
+      return true;
+    }
+
+
+    function buy() public payable returns (bool) {
+      require(msg.sender.balance >= msg.value && msg.value != 0 ether, "ICO: function buy invalid input");
+      uint256 amount = msg.value * 1000;
+      _transfer(owner(), _msgSender(), amount);
+      return true;
+    }
+
+    function withdraw(uint256 amount) public onlyOwner returns (bool) {
+      require(amount <= address(this).balance, "ICO: function withdraw invalid input");
+      payable(_msgSender()).transfer(amount);
+      return true;
     }
 }
